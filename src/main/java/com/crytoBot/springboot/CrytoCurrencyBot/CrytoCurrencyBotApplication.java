c869db229d7f3e6d380ba3c7334d9a854677e221
Abdul Eldarrat <aoe7x5@mail.missouri.edu>
2017-10-23 15:23:29 -0500

    Initial commit for project base

---
+++ src/main/java/com/crytoBot/springboot/CrytoCurrencyBot/CrytoCurrencyBotApplication.java
@@ -0,0 +1,12 @@
+package com.crytoBot.springboot.CrytoCurrencyBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrytoCurrencyBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrytoCurrencyBotApplication.class, args);
+	}
+}
