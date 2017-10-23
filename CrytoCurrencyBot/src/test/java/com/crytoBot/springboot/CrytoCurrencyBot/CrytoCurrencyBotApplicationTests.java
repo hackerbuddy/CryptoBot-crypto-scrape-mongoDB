d05b5ff9f77fffe4244b61fd258c9d4adf962c44
Abdul Eldarrat <aoe7x5@mail.missouri.edu>
2017-10-23 15:04:08 -0500

    Initial commit of project

---
+++ CrytoCurrencyBot/src/test/java/com/crytoBot/springboot/CrytoCurrencyBot/CrytoCurrencyBotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.crytoBot.springboot.CrytoCurrencyBot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrytoCurrencyBotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
